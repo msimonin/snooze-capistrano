64ea8946df3a74bf320b66f70fac1283048f2a14
Matthieu Simonin <matthieu.simonin@inria.fr>
2013-05-01 15:19:32 +0200

    add branch support

---
+++ config/deploy/testing.rb
@@ -0,0 +1,3 @@
+set :snoozenode_deb_url, "https://ci.inria.fr/snooze-software/job/testing-snoozenode/ws/distributions/deb-package/snoozenode_1.1.0-0_all.deb"
+set :snoozeclient_deb_url, "https://ci.inria.fr/snooze-software/job/testing-snoozeclient/ws/distributions/deb-package/snoozeclient_1.1.0-0_all.deb"
+set :snooze_capistrano_branch, "testing"
