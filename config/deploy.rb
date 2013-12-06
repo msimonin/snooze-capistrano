51af6d34dd1601501bb813421476efebb98b2708
msimonin <matthieu.simonin@inria.fr>
2013-12-06 14:42:17 +0100

    fix
    M

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'capistrano/ext/multistage'
 
 # Stages definition
-set :stages, %w(master v2.1.0 maint1.0.0 maint2.0.0 testing sandbox experimental)
+set :stages, %w(master v2.1.0 maint1.0.0 maint2.0.0 sandbox experimental)
 set :default_stage, "v2.1.0"
 
 # Enable pretty output. Remove it if you want full logging
@@ -9,7 +9,7 @@ set :default_stage, "v2.1.0"
 STDOUT.sync
 # Capistrano parameters
 set :g5k_user, "msimonin"
-set :gateway, "#{g5k_user}@grid5000.irisa.fr"
+set :gateway, "#{g5k_user}@access.grid5000.fr"
 # This key will used to access the gateway and nodes
 ssh_options[:keys]= [File.join(ENV["HOME"], ".ssh", "id_rsa"), File.join(ENV["HOME"], ".ssh_insideg5k", "id_rsa"), File.join(ENV["HOME"], ".ssh", "id_rsa_insideg5k")]
 # This key will be installed on nodes
@@ -20,7 +20,7 @@ set :recipes_path, "./recipes"
 
 # G5K global parameters
 set :site, ENV['site'] || "rennes"
-set :walltime, ENV['walltime'] || "1:00:00"
+set :walltime, ENV['walltime'] || "3:00:00"
 set :subnet, ENV['subnet'] || "slash_18"
 set :vlan, ENV['vlan'] || "-1"
 # For nodes reservation, see config/deploy/xp5k
--- config/deploy/master.rb
+++ config/deploy/master.rb
@@ -1,24 +1,25 @@
-#set :snoozenode_deb_url, "http://public.rennes.grid5000.fr/~msimonin/snooze/experimental/snoozenode_exp.deb"
-#set :snoozeclient_deb_url, "http://public.rennes.grid5000.fr/~msimonin/snooze/experimental/snoozeclient_exp.deb"
-
-set :snoozenode_deb_url, "https://ci.inria.fr/snooze-software/job/master-snoozenode/ws/distributions/deb-package/snoozenode_2.1.0-0_all.deb" 
+set :branch, "master"
+set :snoozenode_deb_url, "https://ci.inria.fr/snooze-software/job/master-snoozenode/ws/distributions/deb-package/snoozenode_2.2.0-0_all.deb"
 set :snoozeclient_deb_url, "https://ci.inria.fr/snooze-software/job/master-snoozeclient/ws/distributions/deb-package/snoozeclient_2.1.0-0_all.deb"
-
+set :snoozeimages_deb_url, "https://ci.inria.fr/snooze-software/job/snoozeimages/ws/distributions/deb-package/snoozeimages_2.2.0-0_all.deb"
+set :snoozeec2_deb_url, "https://ci.inria.fr/snooze-software/job/snoozeec2/ws/distributions/deb-package/snoozeec2_2.2.0-0_all.deb"
 
 set :kadeploy3_common_deb_url, "https://gforge.inria.fr/frs/download.php/32874/kadeploy-common-3.1.7.2.deb"
 set :kadeploy3_client_deb_url, "https://gforge.inria.fr/frs/download.php/32873/kadeploy-client-3.1.7.2.deb"
 
 $plugins=[]
+=begin
 $plugins << {
   :url => "https://ci.inria.fr/snooze-software/job/snooze-plugins/ws/random-scheduling/target/random-scheduling-2.0-SNAPSHOT.jar",
   :destination => "/usr/share/snoozenode/plugins/groupManagerScheduler"
 }
+=end
 
-load 'config/deploy/xp5k/xp5k_master.rb'
+load 'config/deploy/xp5k/xp5k_2.x.rb'
 
 # load recipes
 # don't forget to change stage to install specific version
-recipes = ["rabbitmq","cassandra", "nfs", "snooze", "snooze_webinterface"]
+recipes = ["rabbitmq","cassandra", "nfs", "snooze"]
 
 recipes.each do |recipe|
   load "#{recipes_path}/#{recipe}/recipe.rb"
@@ -29,4 +30,4 @@ task :automatic do
  puts "Welcome to Snooze deployment".bold.blue
 end
 
-after "automatic", "xp5k", "rabbitmq","cassandra", "nfs", "snooze", "snooze_webinterface"
+after "automatic", "xp5k", "rabbitmq","cassandra", "snooze", "nfs"
